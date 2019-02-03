33d7b63603bb576bb8774c837695f2b10b7f7957
Youngwook <30427798+yk287@users.noreply.github.com>
2019-02-02 22:54:32 -0800

    Add files via upload

---
+++ LSGansTF/main.py
@@ -0,0 +1,92 @@
+
+from util import *
+from model import *
+from dataloader import *
+
+from options import options
+
+options = options()
+opts = options.parse()
+
+tf.reset_default_graph()
+
+# number of images for each batch
+batch_size = opts.batch
+# our noise dimension
+noise_dim = 96
+
+# placeholder for images from the training dataset
+x = tf.placeholder(tf.float32, [None, 784])
+# random noise fed into our generator
+z = sample_noise(batch_size, noise_dim)
+# generated images
+G_sample = generator(z)
+
+with tf.variable_scope("") as scope:
+    #scale images to be -1 to 1
+    logits_real = discriminator(preprocess_img(x))
+    # Re-use discriminator weights on new inputs
+    scope.reuse_variables()
+    logits_fake = discriminator(G_sample)
+
+# Get the list of variables for the discriminator and generator
+D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
+G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'generator')
+
+# get our solver
+D_solver, G_solver = get_solvers(learning_rate=opts.lr)
+
+# get our loss
+D_loss, G_loss = gan_loss(logits_real, logits_fake)
+
+# setup training steps
+D_train_step = D_solver.minimize(D_loss, var_list=D_vars)
+G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
+
+D_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'discriminator')
+G_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'generator')
+
+
+# a giant helper function
+def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_extra_step, \
+              show_every=2, print_every=1, batch_size=128, num_epoch=10):
+    """Train a GAN for a certain number of epochs.
+
+    Inputs:
+    - sess: A tf.Session that we want to use to run our data
+    - G_train_step: A training step for the Generator
+    - G_loss: Generator loss
+    - D_train_step: A training step for the Generator
+    - D_loss: Discriminator loss
+    - G_extra_step: A collection of tf.GraphKeys.UPDATE_OPS for generator
+    - D_extra_step: A collection of tf.GraphKeys.UPDATE_OPS for discriminator
+    Returns:
+        Nothing
+    """
+    # compute the number of iterations we need
+    mnist = MNIST(batch_size=batch_size, shuffle=True)
+    for epoch in range(num_epoch):
+        # every show often, show a sample result
+        if epoch % show_every == 0:
+            samples = sess.run(G_sample)
+            fig = show_images(samples[:16])
+            plt.show()
+            print()
+        for (minibatch, minbatch_y) in mnist:
+            # run a batch of data through the network
+            _, D_loss_curr = sess.run([D_train_step, D_loss], feed_dict={x: minibatch})
+            _, G_loss_curr = sess.run([G_train_step, G_loss])
+
+        # print loss every so often.
+        # We want to make sure D_loss doesn't go to 0
+        if epoch % print_every == 0:
+            print('Epoch: {}, D: {:.4}, G:{:.4}'.format(epoch, D_loss_curr, G_loss_curr))
+    print('Final images')
+    samples = sess.run(G_sample)
+
+    fig = show_images(samples[:16])
+    plt.show()
+
+with get_session() as sess:
+    sess.run(tf.global_variables_initializer())
+    run_a_gan(sess,G_train_step,G_loss,D_train_step,D_loss,G_extra_step,D_extra_step, num_epoch=opts.epoch)
---
+++ LSGansTF/tf_util.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+
+
+def get_session():
+    '''
+    makes it so that tf session can allocate more gpu memory as needed
+    :return:
+    '''
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True #allocates more gpu memory as needed during run time
+    session = tf.Session(config=config)
+    return session
+
+
+def leaky_relu(x, alpha=0.01):
+    """Compute the leaky ReLU activation function.
+
+    Inputs:
+    - x: TensorFlow Tensor with arbitrary shape
+    - alpha: leak parameter for leaky ReLU
+
+    Returns:
+    TensorFlow Tensor with the same shape as x
+    """
+    return tf.maximum(x, tf.multiply(x, alpha))
+
+def sample_noise(batch_size, dim):
+    """Generate random uniform noise from -1 to 1.
+
+    Inputs:
+    - batch_size: integer giving the batch size of noise to generate
+    - dim: integer giving the dimension of the the noise to generate
+
+    Returns:
+    TensorFlow Tensor containing uniform noise in [-1, 1] with shape [batch_size, dim]
+    """
+
+    return tf.random_uniform([batch_size, dim], minval=-1, maxval=1)
+
+def gan_loss(logits_real, logits_fake):
+    """Compute the GAN loss.
+
+    Inputs:
+    - logits_real: Tensor, shape [batch_size, 1], output of discriminator
+        Unnormalized score that the image is real for each real image
+    - logits_fake: Tensor, shape[batch_size, 1], output of discriminator
+        Unnormalized score that the image is real for each fake image
+
+    Returns:
+    - D_loss: discriminator loss scalar
+    - G_loss: generator loss scalar
+
+    HINT: for the discriminator loss, you'll want to do the averaging separately for
+    its two components, and then add them together (instead of averaging once at the very end).
+    """
+
+    G_loss = tf.reduce_mean(tf.losses.mean_squared_error(labels=tf.ones_like(logits_fake), predictions=logits_fake))
+    D_loss = tf.reduce_mean(tf.losses.mean_squared_error(labels=tf.ones_like(logits_real), predictions=logits_real)) \
+             + tf.reduce_mean(tf.losses.mean_squared_error(labels=tf.zeros_like(logits_fake), predictions=logits_fake))
+
+    return D_loss, G_loss
+
+def get_solvers(learning_rate=1e-3, beta1=0.5):
+    """Create solvers for GAN training.
+
+    Inputs:
+    - learning_rate: learning rate to use for both solvers
+    - beta1: beta1 parameter for both solvers (first moment decay)
+
+    Returns:
+    - D_solver: instance of tf.train.AdamOptimizer with correct learning_rate and beta1
+    - G_solver: instance of tf.train.AdamOptimizer with correct learning_rate and beta1
+    """
+
+    D_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+    G_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+    return D_solver, G_solver
