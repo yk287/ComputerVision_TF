d23a863772d00c1c7a50d607cff40b0826cd985d
Youngwook <30427798+yk287@users.noreply.github.com>
2019-02-12 22:51:02 -0800

    Add files via upload

--- CGANTF/util.py
+++ CGANTF/util.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+
 plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-def show_images(images):
+def show_images(images, name='image'):
     images = np.reshape(images, [images.shape[0], -1])  # images reshape to (batch_size, D)
     sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
     sqrtimg = int(np.ceil(np.sqrt(images.shape[1])))
@@ -23,6 +24,7 @@ def show_images(images):
         ax.set_yticklabels([])
         ax.set_aspect('equal')
         plt.imshow(img.reshape([sqrtimg,sqrtimg]))
+        plt.savefig(name)
     return
 
 def preprocess_img(x):
@@ -34,38 +36,18 @@ def deprocess_img(x):
 def rel_error(x,y):
     return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
-def one_hot_encoder(index_array, n_classes = 10):
+def black_out(image, width=3):
     '''
-    One hot encoder that takes in an array of labels (ex.  array([1, 0, 3])) and returns
-    a n-dimensional array that is one hot encoded
-    :param index_array (array of ints): an array that holds class labels
-    :param n_classes (int): number of classes
+    Blacks out everything in the image except for the top left quadrant
+    :param image: Numpy array
     :return:
     '''
 
-    batch_size = index_array.shape[0]
+    h = image.shape[1]
+    w = image.shape[2] // 2
 
-    b = np.zeros((batch_size, n_classes))
-    b[np.arange(batch_size), index_array] = 1
-    return np.float32(b)
+    mask = np.zeros(image.shape, np.float32)
 
-def categorical_label_generator(batch_size=128, n_classes=10):
-    '''
-    Function that returns an array with a label between [0, 10] for an element with size batch_size
-    :param batch_size (int): length of an array
-    :param n_classes (int): total number of classes
-    :return:
-    '''
+    mask[:, 0: h, w - width: w + width, :] = image[:, 0: h, w - width: w + width, :]
 
-    array = np.random.choice(n_classes, batch_size)
-
-    return array
-
-def generate_fake_label(batch_size=128, n_classes=10):
-
-    fake_labels = categorical_label_generator(batch_size, n_classes=n_classes)
-    fake_labels = one_hot_encoder(fake_labels, n_classes)
-
-    return fake_labels
-
-
+    return mask
