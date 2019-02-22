e7009ab7fb8b7ce28c78f91ab262866e7d660d48
Youngwook <30427798+yk287@users.noreply.github.com>
2019-02-21 23:05:08 -0800

    Add files via upload

--- CycleGan_TF/options.py
+++ CycleGan_TF/options.py
@@ -13,9 +13,9 @@ class options():
         self.parser.add_argument('--lamb', type=int, nargs='?', default=10, help='lambda for cycle')
 
         #Image
-        self.parser.add_argument('--resize', type=int, nargs='?', default=224, help='Image Resize size')
+        self.parser.add_argument('--resize', type=int, nargs='?', default=144, help='Image Resize size')
         self.parser.add_argument('--num_images', type=int, nargs='?', default=2, help='number of images in a single image file')
-        self.parser.add_argument('--image_shape', type=int, nargs='?', default=196, help='height of a square image')
+        self.parser.add_argument('--image_shape', type=int, nargs='?', default=128, help='height of a square image')
         self.parser.add_argument('--channel_in', type=int, nargs='?', default=3, help='number of input channels')
         self.parser.add_argument('--channel_out', type=int, nargs='?', default=3, help='number of output channels')
         self.parser.add_argument('--channel_up', type=int, nargs='?', default=8, help='initial channel increasing')
@@ -27,8 +27,8 @@ class options():
         self.parser.add_argument('--save_progress', type=bool, nargs='?', default=True,
                                  help='save training progress')
         self.parser.add_argument('--memory_len', type=int, nargs='?', default=50, help='size of memory from which the Discriminator is used to update')
-        self.parser.add_argument('--show_every', type=int, nargs='?', default=1500, help='how often to show images')
-        self.parser.add_argument('--print_every', type=int, nargs='?', default=150, help='how often to print losses')
+        self.parser.add_argument('--show_every', type=int, nargs='?', default=15000, help='how often to show images')
+        self.parser.add_argument('--print_every', type=int, nargs='?', default=3000, help='how often to print losses')
         self.parser.add_argument('--const_epoch', type=int, nargs='?', default=100, help='number of epochs where LR is constant')
         self.parser.add_argument('--adaptive_epoch', type=int, nargs='?', default=100, help='number of epochs where LR changes')
 
